3a9f7a83a47adf1a301a577be07acd07eb70b22d
Zababurin <zababurinsv@vk.com>
2024-08-20 16:38:43 +0300

    update

--- relay.js
+++ relay.js
@@ -70,7 +70,7 @@ async function main () {
   const node = await createLibp2p({
     peerId,
     addresses: {
-      listen: [`/ip4/circuit-relay.onrender.com/tcp/${port}/ws`],
+      listen: [`/ip4/0.0.0.0/tcp/443/wss`],
       announce: [`/dns4/circuit-relay.onrender.com/tcp/${port}/wss`],
       // TODO check "What is next?" section
       // announce: ['/dns4/auto-relay.libp2p.io/tcp/443/wss/p2p/QmWDn2LY8nannvSWJzruUYoLZ4vV83vfCBwd8DipvdgQc3']
@@ -184,10 +184,10 @@ async function main () {
 
   app.use(queue.getErrorMiddleware());
 
-  // app.listen(port, () => {
-  //   console.log('pid: ', process.pid);
-  //   console.log('listening on http://localhost:' + port);
-  // });
+  app.listen(port, () => {
+    console.log('pid: ', process.pid);
+    console.log('listening on http://localhost:' + port);
+  });
 }
 
 main()
