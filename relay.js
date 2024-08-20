2f33b1b11e822634768a149764292e4b293646a2
Zababurin <zababurinsv@vk.com>
2024-08-20 03:10:10 +0300

    add path

--- relay.js
+++ relay.js
@@ -75,7 +75,11 @@ async function main () {
 
   console.log(`Node started with id ${node.peerId.toString()}`)
   console.log('Listening on:')
-  node.getMultiaddrs().forEach((ma) => console.log(ma.toString()))
+  let pathNode = ''
+  node.getMultiaddrs().forEach((ma) => {
+    pathNode = ma.toString()
+    console.log(pathNode)
+  })
 
 
   app.get(`/env.json`, async (req, res) => {
@@ -87,8 +91,73 @@ async function main () {
   })
 
   app.get(`/*`, async (req, res) => {
-    // console.log('index ----- index', __dirname)
-    res.status(200).sendFile(path.join(__dirname, '/index.html'));
+    const html = `<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>org browser relay</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes" />
+    <meta
+            name="description"
+            content="">
+    <meta property="og:site_name" content="markdown" />
+    <meta property="og:locale" content="ru_RU" />
+    <meta property="og:type" content="contract" />
+    <meta property="og:title" content="markdown" />
+    <meta property="og:description" content="markdown" />
+    <meta property="og:image" content="https://i.imgur.com/pSrPUkJ.jpg" />
+    <meta property="og:image:width" content="537" />
+    <meta property="og:image:height" content="240" />
+    <link rel="shortcut icon"
+          href="data:image/png;base64, AAABAAEAEBAAAAEAIABoBAAAFgAAACgAAAAQAAAAIAAAAAEAIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAbbv+DGW3/mRlt/5kZbf+ZGq6/hIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGa3/ohkt/7/Zbj//2S3/v9lt/6WAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGm5/iRlt/74Zbj//2W4//9luP//Zbf++mi4/i4gIPciGhr24hsb9uwbG/bsGhr24CEh9xoAAAAAAAAAAAAAAABnuP5mZLf+/2W4//9luP//Zbj//2S3/v9muP5yGBj2rhMT9v8TE/b/ExP2/xMT9f8YGPWkAAAAAAAAAAAAAAAAb7z/BGW3/tZluP//Zbj//2W4//9lt/7gJzH3ShMT9f8TE/b/ExP2/xMT9v8TE/b/ExP1/CAg9joAAAAAAAAAAAAAAABmuP5GZLf+6GS3/uhkt/7oZbf+UhgY9YQSEvX/ExP2/xMT9v8TE/b/ExP2/xIS9f8aGvZ8AAAAAD4++gQgIPZ6IiL2hiIi9oYgIPZ8KCj5BAAAAAAtLfgUFBT17BMT9v8TE/b/ExP2/xMT9v8VFfXoLCz4DgAAAAAaGvZqEhL1/xMT9v8TE/b/EhL1/xsb9nIAAAAAAAAAABwc9m4SEvX/ExP2/xMT9v8SEvX/HR32ZAAAAAAnJ/gSFRX16hMT9v8TE/b/ExP2/xMT9v8UFPXuJyf4Fp2xlAKNnqUYLC/mfhYW83ATE/VuFxf1aDc3+gIAAAAAGBj1fhIS9f8TE/b/ExP2/xMT9v8TE/b/ExP1/xkZ9YaGn3yIhZ57/4Wee/+Gn3yKAAAAAAAAAAAAAAAAAAAAACMj9zYTE/X8ExP2/xMT9v8TE/b/ExP2/xMT9f9JUshihZ57+IaffP+Gn3z/hZ579oigfiYAAAAAAAAAAAAAAAAAAAAAGBj1oBIS9f8TE/b/ExP2/xMT9f8YGPWmiKB+PIWee/+Gn3z/hp98/4Wee/+HoH06AAAAAAAAAAAAAAAAAAAAACUl9xgVFfXOExP11BMT9dQUFPXQJib3HgAAAACGn3ymhp98/4affP+Gn3ymAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAiKB+EIihf0CIoX9AiKB+EAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA//8AAP//AADg/wAA4MMAAOCBAADggQAA8QEAAOeBAADDwwAAgf8AAIAPAACBDwAAgQ8AAMMPAAD//wAA//8AAA=="
+          type="image/png">
+    <style>
+      .body {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        margin: auto;
+      }
+      .body img {
+        align-self: center;
+      }
+
+      #addr {
+        display: flex;
+        flex-direction: row;
+        gap: 24px;
+        justify-content: center;
+        align-items: center;
+      }
+
+      p {
+        background: #AFAFAF;
+        padding: 4px;
+        border-radius: 2px;
+        width: max-content;
+      }
+    </style>
+  </head>
+  <body>
+  <div class="body">
+    <br>
+    <img src="./newkind-icon-512-maskable.png" alt="org Logo" width="128">
+    <h2>This is a relay</h2>
+    <div id="addr">You can add this bootstrap list with the address <p>${pathNode}</p></div>
+  </div>
+  </body>
+</html>
+`;
+    res.status(200).send(html);
+    // res.status(200).sendFile(path.join(__dirname, '/index.html'));
   });
 
   app.post(`/*`, async (req, res) => {
