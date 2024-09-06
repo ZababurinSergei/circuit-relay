0b15bb5007ebcbd65495d0d5826808c64e2eecd3
Забабурин Сергей <zababurins@vk.com>
2024-09-06 22:01:42 +0300

    update

--- relay.js
+++ relay.js
@@ -164,7 +164,7 @@ async function main () {
 
     let adresses = process.env.PORT
     ? {
-      listen: [`/ip4/0.0.0.0/tcp/${port}/ws`],
+      listen: [`/ip4/0.0.0.0/tcp/${port}/wss`],
       announce: [
         `/dns4/${process.env.RENDER_EXTERNAL_HOSTNAME}/tcp/${port}/wss/p2p/${peerId.toString()}`
       ]
